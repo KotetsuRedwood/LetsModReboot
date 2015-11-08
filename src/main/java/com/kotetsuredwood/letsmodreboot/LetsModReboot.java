732dfd44bdb364849c490946ae180372b8ceec8d
KotetsuRedwood <kotetsu.redwood@hotmail.co.uk>
2015-11-08 00:30:33 +0000

    mcmod.info + main mod class
    More basic set up I guess!

---
+++ src/main/java/com/kotetsuredwood/letsmodreboot/LetsModReboot.java
@@ -0,0 +1,32 @@
+package com.kotetsuredwood.letsmodreboot;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="LetsModReboot",name = "Let's Mod Reboot", version = "1.0(1.7.10)")
+public class LetsModReboot
+{
+    @Mod.Instance("LetsModReboot")
+    public static LetsModReboot instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
